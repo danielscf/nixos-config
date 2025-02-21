554cfd8bedb6a0eecb548f8af15675abc441ca3d
danielscf <fdanielsantacruz@gmail.com>
2025-02-21 17:04:07 -0500

    Bootloader log level 3

--- modules/core/grub.nix
+++ modules/core/grub.nix
@@ -3,17 +3,22 @@
   # TODO: Ask if efi/bios
   # TODO: Ask if encryption
   # TODO: Ask if OSProber
-  boot.loader = {
-    grub = {
-      enable = true;
-      device = "nodev";
-      useOSProber = true;
-      efiSupport = true;
-      enableCryptodisk = false;
-    };
-    efi = {
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot";
+  boot = {
+    kernelParms = [
+      "loglevel=3"
+    ];
+    loader = {
+      grub = {
+        enable = true;
+        device = "nodev";
+        useOSProber = true;
+        efiSupport = true;
+        enableCryptodisk = false;
+      };
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
     };
   };
 }
