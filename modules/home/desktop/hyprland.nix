be87330125459b8fa0eeb23fa9642abad5db2ea3
danielscf <fdanielsantacruz@gmail.com>
2025-02-22 13:45:49 -0500

    temp enabled laptop monitor

--- modules/home/desktop/hyprland.nix
+++ modules/home/desktop/hyprland.nix
@@ -17,7 +17,7 @@
 
       monitor = [
         ",preferred,auto,auto"
-        "$laptop_monitor,disable"
+        # "$laptop_monitor,disable"
         "$primary_monitor,1920x1080@165,0x0,1"
         "$secondary_monitor,1366x768@60,auto-left,1,transform,1"
       ];
