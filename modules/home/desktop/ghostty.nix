da2cf006e3cb994c2d01e59bbcabd66ef27d1484
danielscf <fdanielsantacruz@gmail.com>
2025-02-24 19:37:43 -0500

    Disabled ligatures ghostty

--- modules/home/desktop/ghostty.nix
+++ modules/home/desktop/ghostty.nix
@@ -7,6 +7,8 @@
     enable = true;
     clearDefaultKeybinds = true;
     settings = {
+      font-feature = "-calt, -liga, -dLig";
+
       window-padding-x = 12;
       window-padding-y = 8;
       window-padding-balance = true;
