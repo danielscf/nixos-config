14cc0653729a0835d5a345446ffcf7416f98490f
danielscf <fdanielsantacruz@gmail.com>
2025-02-24 19:08:40 -0500

    Adjusted padding ghostty

--- modules/home/desktop/ghostty.nix
+++ modules/home/desktop/ghostty.nix
@@ -7,8 +7,8 @@
     enable = true;
     clearDefaultKeybinds = true;
     settings = {
-      window-padding-x = 8;
-      window-padding-y = 10;
+      window-padding-x = 12;
+      window-padding-y = 8;
       window-padding-balance = true;
       window-padding-color = "extend";
       window-decoration = "none";
