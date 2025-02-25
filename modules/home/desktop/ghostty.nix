2720dc171cf2a356c5dee0b8ab05b8d3fc13c3ec
danielscf <fdanielsantacruz@gmail.com>
2025-02-24 19:02:40 -0500

    Ghostty settings

--- modules/home/desktop/ghostty.nix
+++ modules/home/desktop/ghostty.nix
@@ -1,7 +1,17 @@
-{ config, pkgs, ... }:
-
 {
+  config,
+  pkgs,
+  ...
+}: {
   programs.ghostty = {
     enable = true;
+    clearDefaultKeybinds = true;
+    settings = {
+      window-padding-x = 8;
+      window-padding-y = 8;
+      window-padding-balance = true;
+      window-padding-color = "extend";
+      window-decoration = "none";
+    };
   };
 }
