1fbf0d2cc623dc5bad14817d044b0d9a72bc35fd
danielscf <fdanielsantacruz@gmail.com>
2025-02-27 00:17:56 -0500

    Nvidia drm fbdev and modeset

--- modules/core/nvidia.nix
+++ modules/core/nvidia.nix
@@ -11,7 +11,10 @@
 
   boot = {
     initrd.kernelModules = ["nvidia" "i915" "nvidia_modeset" "nvidia_uvm" "nvidia_drm"];
-    kernelParams = ["nvidia-drm.fbdev=1"];
+    kernelParams = [
+      "nvidia-drm.fbdev=1"
+      "nvidia-drm.modeset=1"
+    ];
   };
 
   environment = {
