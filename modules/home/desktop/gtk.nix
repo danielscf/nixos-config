ac06ddbc463a160b400105bea23eaa0dc3cf95a8
danielscf <fdanielsantacruz@gmail.com>
2025-02-27 13:57:54 -0500

    Gtk ensure directories

--- modules/home/desktop/gtk.nix
+++ modules/home/desktop/gtk.nix
@@ -1,22 +1,32 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
+  home.activation.ensureDirs = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    mkdir -p ~/Pictures/screenshots \
+      ~/Pictures/wallpapers \
+      ~/Videos/recording \
+      ~/nixos-config \
+      ~/projects \
+      ~/practice
+  '';
+
   gtk = {
     enable = true;
     gtk3.bookmarks = [
       "file:///home/daniel/Downloads"
-      "file:///run/media/daniel/EXTERNAL/"
+      "file:///home/daniel/Documents"
       "file:///home/daniel/Pictures"
       "file:///home/daniel/Videos"
+      "file:///run/media/daniel/EXTERNAL/"
       "file:///home/daniel/Videos/recording"
       "file:///home/daniel/Pictures/screenshots"
       "file:///home/daniel/Pictures/wallpapers"
+      "file:///home/daniel/nixos-config/"
       "file:///home/daniel/projects"
       "file:///home/daniel/practice"
-      "file:///home/daniel/.config"
-      "file:///home/daniel/.cache"
     ];
   };
 }
