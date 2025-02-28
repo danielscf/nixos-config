385064b709580a454b52c938e23fb51f151d5dff
danielscf <fdanielsantacruz@gmail.com>
2025-02-28 16:22:46 -0500

    External drive bind mounting options

--- modules/core/external_drive.nix
+++ modules/core/external_drive.nix
@@ -19,6 +19,16 @@
   fileSystems."/home/daniel/notes" = {
     device = "/mnt/external/vaults/personal";
     fsType = "none";
-    options = ["bind" "x-systemd.automount" "x-systemd.idle-timeout=600"];
+    options = [
+      "bind"
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=600"
+      "users"
+      "nofail"
+      "noatime"
+      "gid=100"
+      "uid=1000"
+      "windows_names"
+    ];
   };
 }
