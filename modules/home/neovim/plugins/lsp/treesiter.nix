4bc1676971541d0907af69c8a12d948f682ed4f3
danielscf <fdanielsantacruz@gmail.com>
2025-03-01 21:56:18 +0000

    Fixed nixvim treesiter

--- modules/home/neovim/plugins/lsp/treesiter.nix
+++ modules/home/neovim/plugins/lsp/treesiter.nix
@@ -1,90 +1,26 @@
+{pkgs, ... }:
+
 {
   programs.nixvim.plugins.treesiter = {
     enable = true;
     autoLoad = true;
     folding = true;
 
-    highlight.enable = true;
-    indent.enable = true;
+    nixGrammar = true;
+    grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
 
-    incremental_selection = {
-      enable = true;
-      keymaps = {
-	init_selection = "<C-space>";
-	node_decremental = "<C-space>";
-	node_incremental = "<bs>";
-	scope_incremental = false;
+    settings = {
+      highlight.enable = true;
+      indent.enable = true;
+      incremental_selection = {
+	enable = true;
+	keymaps = {
+	  init_selection = "<C-space>";
+	  node_decremental = "<C-space>";
+	  node_incremental = "<bs>";
+	  scope_incremental = false;
+	};
       };
     };
-
-    grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
-      astro
-      bash
-      c
-      c_sharp
-      cmake
-      cpp
-      css
-      dart
-      diff
-      dockerfile
-      elixir
-      fish
-      gitignore
-      go
-      godot_resource
-      gomod
-      gosum
-      gowork
-      gpg
-      graphql
-      groovy
-      haskell
-      haskell_persistent
-      hjson
-      html
-      htmldjango
-      http
-      hyprlang
-      java
-      javascript
-      jsdoc
-      json
-      json5
-      jsonc
-      kdl
-      kotlin
-      lua
-      luadoc
-      luap
-      make
-      markdown
-      markdown-inline
-      nix
-      ocaml
-      ocaml_interface
-      perl
-      php
-      phpdoc
-      prisma
-      python
-      query
-      rasi
-      regex
-      ruby
-      rust
-      scala
-      sql
-      svelte
-      terraform
-      toml
-      tsx
-      typescript
-      vim
-      vue
-      vimdoc
-      xml
-      yaml
-    ];
   };
 }
