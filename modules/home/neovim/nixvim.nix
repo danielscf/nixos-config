0e0748681bc19381b06d651b1b28c6d7034a49e3
danielscf <fdanielsantacruz@gmail.com>
2025-03-01 00:44:52 -0500

    Basic plugins nixvim

--- modules/home/neovim/nixvim.nix
+++ modules/home/neovim/nixvim.nix
@@ -26,6 +26,70 @@
       shiftwidth = 2;
     };
 
+    plugins = {
+      auto-session = {
+	enable = true;
+	autoLoad = true;
+	lazyLoad.enable = false;
+      };
+
+      neoscroll = {
+        enable = true;
+	autoLoad = true;
+      };
+
+      tmux-navigator = {
+	enable = true;
+	autoLoad = true;
+	lazyLoad.enable = false;
+	keymaps = [
+	  {
+	    action = "left";
+	    key = "<C-h>";
+	    mode = ["n"];
+	  }
+	  {
+	    action = "down";
+	    key = "<C-j>";
+	    mode = ["n"];
+	  }
+	  {
+	    action = "up";
+	    key = "<C-k>";
+	    mode = ["n"];
+	  }
+	  {
+	    action = "right";
+	    key = "<C-l>";
+	    mode = ["n"];
+	  }
+	];
+      };
+
+      which-key = {
+	enable = true;
+	autoLoad = true;
+	lazyLoad.enable = false;
+	settings = {
+	  delay = 500;
+	  preset = "classic";
+	};
+      };
+
+      oil = {
+	enable = true;
+	autoLoad = false;
+	settings = {
+	  delete_to_trash = true;
+	  default_file_explorer = true;
+	  skip_confirm_for_simple_edits = true;
+	};
+      };
+
+      nvim-surround.enable = true;
+      vim-be-good.enable = true;
+    };
+
     keymaps = [
       {
         key = ";";
