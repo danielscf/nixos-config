f3dcb61421e1b7d5bacd62a34f8b00835adf679c
danielscf <fdanielsantacruz@gmail.com>
2025-03-01 11:02:58 -0500

    Nixvim default plugins module

--- modules/home/neovim/nixvim.nix
+++ modules/home/neovim/nixvim.nix
@@ -1,17 +1,11 @@
 {inputs, ...}: {
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
+
     ./options.nix
     ./keymaps.nix
 
-    ./plugins/oil.nix
-    ./plugins/which-key.nix
-    ./plugins/auto-session.nix
-    ./plugins/tmux-navigator.nix
-    ./plugins/neoscroll.nix
-    ./plugins/nvim-surround.nix
-    ./plugins/vim-be-good.nix
-    ./plugins/fugitive.nix
+    ./plugins
   ];
 
   programs.nixvim = {
---
+++ modules/home/neovim/plugins/default.nix
@@ -0,0 +1,12 @@
+{
+  imports = [
+    ./oil.nix
+    ./which-key.nix
+    ./auto-session.nix
+    ./tmux-navigator.nix
+    ./neoscroll.nix
+    ./nvim-surround.nix
+    ./vim-be-good.nix
+    ./fugitive.nix
+  ];
+}
