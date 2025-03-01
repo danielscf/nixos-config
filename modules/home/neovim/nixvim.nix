bbbf7b894bafb61d0ed1f9be10afb5ab2c378df9
danielscf <fdanielsantacruz@gmail.com>
2025-02-28 23:19:29 -0500

    Basic nixvim config

--- modules/home/neovim/nixvim.nix
+++ modules/home/neovim/nixvim.nix
@@ -6,5 +6,31 @@
   programs.nixvim = {
     enable = true;
     defaultEditor = true;
+
+    enableMan = true;
+    vimAlias = true;
+    clipboard.register = "unnamedplus";
+
+    withNodejs = true;
+    withPerl = true;
+    withPython3 = true;
+    withRuby = true;
+    
+    globals = {
+      mapleader = " ";
+    };
+
+    opts = {
+      number = true;
+      relativenumber = true;
+      shiftwidth = 2;
+    };
+
+    keymaps = [
+      {
+        key = ";";
+	action = ":";
+      }
+    ];
   };
 }
