6a76bbd171f20a50491fc95086d9779b06e80711
danielscf <fdanielsantacruz@gmail.com>
2025-03-01 01:27:54 -0500

    Nixvim oil keymap

--- modules/home/neovim/plugins/oil.nix
+++ modules/home/neovim/plugins/oil.nix
@@ -1,12 +1,22 @@
 {
-  programs.nixvim.plugins.oil = {
-    enable = true;
-    autoLoad = true;
-    settings = {
-      delete_to_trash = true;
-      default_file_explorer = true;
-      skip_confirm_for_simple_edits = true;
-      use_default_keymaps = true;
+  programs.nixvim = {
+    plugins.oil = {
+      enable = true;
+      autoLoad = true;
+      settings = {
+	delete_to_trash = true;
+	default_file_explorer = true;
+	skip_confirm_for_simple_edits = true;
+	use_default_keymaps = true;
+      };
     };
+
+    keymaps = [
+      {
+        key = "-";
+	action = "<cmd>Oil<cr>";
+	mode = ["n"];
+      }
+    ];
   };
 }
