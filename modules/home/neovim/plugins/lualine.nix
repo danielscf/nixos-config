92114d4441277870d5072a0d0283e25e93306029
danielscf <fdanielsantacruz@gmail.com>
2025-03-02 03:18:14 +0000

    Nixvim stylix theme lualine

--- modules/home/neovim/plugins/lualine.nix
+++ modules/home/neovim/plugins/lualine.nix
@@ -1,4 +1,7 @@
 {
+  config,
+  ...
+}: {
   programs.nixvim.plugins.lualine = {
     enable = true;
     autoLoad = true;
@@ -45,10 +48,10 @@
 	      left = 1;
 	      right = 0;
 	    };
-            # color = {
-            #   fg = require("lualine.themes.lushwal").normal.c.fg;
-            #   bg = require("lualine.themes.lushwal").normal.c.bg;
-            # };
+            color = {
+              fg = "${config.lib.stylix.colors.base06}";
+              bg = "${config.lib.stylix.colors.base00}";
+            };
           }
           {
             _unkeyed-1 = "filename";
@@ -59,24 +62,24 @@
               newfile = "[New]";
             };
             path = 1;
-            # color = {
-            #   fg = require("lualine.themes.lushwal").normal.c.fg,
-            #   bg = require("lualine.themes.lushwal").normal.c.bg,
-            # };
+            color = {
+              fg = "${config.lib.stylix.colors.base06}";
+              bg = "${config.lib.stylix.colors.base00}";
+            };
           }
         ];
         lualine_b = [
           {
             _unkeyed-1 = "diagnostics";
-            # color = {
-            #   fg = require("lualine.themes.lushwal").normal.c.fg,
-            #   bg = require("lualine.themes.lushwal").normal.c.bg,
-            # },
             symbols = {
               error = " ";
               warn = " ";
               info = " ";
               hint = " ";
+            };
+            color = {
+              fg = "${config.lib.stylix.colors.base06}";
+              bg = "${config.lib.stylix.colors.base00}";
             };
           }
         ];
@@ -94,44 +97,44 @@
         lualine_y = [
           {
             _unkeyed-1 = "diff";
-            # color = {
-            #   fg = require("lualine.themes.lushwal").normal.c.fg,
-            #   bg = require("lualine.themes.lushwal").normal.c.bg,
-            # },
             symbols = {
               added = " ";
               modified = " ";
               removed = " ";
             };
+            color = {
+              fg = "${config.lib.stylix.colors.base06}";
+              bg = "${config.lib.stylix.colors.base00}";
+            };
           }
           {
             _unkeyed-1 = "branch";
-            # color = {
-            #   fg = require("lualine.themes.lushwal").normal.c.fg,
-            #   bg = require("lualine.themes.lushwal").normal.c.bg,
-            # },
+            color = {
+              fg = "${config.lib.stylix.colors.base06}";
+              bg = "${config.lib.stylix.colors.base00}";
+            };
           }
         ];
         lualine_z = [
           {
             _unkeyed-1 = "mode";
-            # color = {
-            #   fg = require("lualine.themes.lushwal").normal.c.fg,
-            #   bg = require("lualine.themes.lushwal").normal.c.bg,
-            # },
+            color = {
+              fg = "${config.lib.stylix.colors.base06}";
+              bg = "${config.lib.stylix.colors.base00}";
+            };
           }
         ];
       };
-      inactive_sections = {
-        lualine_a = {};
-        lualine_b = {};
-        lualine_c = {};
-        lualine_x = {};
-        lualine_y = {};
-        lualine_z = {};
-      };
-      winbar = {};
-      inactive_winbar = {};
+      # inactive_sections = {
+      #   lualine_a = {};
+      #   lualine_b = {};
+      #   lualine_c = {};
+      #   lualine_x = {};
+      #   lualine_y = {};
+      #   lualine_z = {};
+      # };
+      # winbar = {};
+      # inactive_winbar = {};
     };
   };
 }
