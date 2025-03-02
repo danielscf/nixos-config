1a1edcf3b29d2d0fe86e46f27b659b7a85b2e553
danielscf <fdanielsantacruz@gmail.com>
2025-03-02 20:46:25 +0000

    Nixvim blink rust binaries

--- modules/home/neovim/plugins/lsp/blink-cmp.nix
+++ modules/home/neovim/plugins/lsp/blink-cmp.nix
@@ -1,13 +1,16 @@
 {
+  # TODO: Add cargo pkg dependency option
   programs.nixvim.plugins.blink-cmp = {
     enable = true;
     autoLoad = true;
+    
     settings = {
       completion = {
 	documentation = {
 	  auto_show = true;
 	};
       };
+      fuzzy.implementation = "prefer_rust";
       signature.enabled = true;
     };
   };
