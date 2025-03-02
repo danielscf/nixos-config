e84e50e254aa73e779ce941ecfa7d5971d5ca29b
danielscf <fdanielsantacruz@gmail.com>
2025-03-02 20:48:44 +0000

    Nixvim lsp blink capabilities

--- modules/home/neovim/plugins/lsp/lsp.nix
+++ modules/home/neovim/plugins/lsp/lsp.nix
@@ -3,6 +3,11 @@
     plugins.lsp = {
       enable = true;
       autoLoad = true;
+
+      capabilities = ''
+	capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)	
+      '';
+
       servers = {
 	nixd = {
 	  enable = true;
