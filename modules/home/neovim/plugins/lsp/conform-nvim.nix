172bb65687e3873a52e91dc2be2ff46768a9e704
danielscf <fdanielsantacruz@gmail.com>
2025-03-02 22:47:03 +0000

    Nixvim formatter pkgs

--- modules/home/neovim/plugins/lsp/conform-nvim.nix
+++ modules/home/neovim/plugins/lsp/conform-nvim.nix
@@ -1,36 +1,38 @@
-{lib, pkgs, ...}:
+{ lib, pkgs, ... }:
 {
   programs.nixvim.plugins.conform-nvim = {
     enable = true;
     autoLoad = true;
     settings = {
       formatters_by_ft = {
-	bash = ["shellcheck" "shellharden"];
-        sh =  [ "shellharden" ];
-        javascript =  [ "prettierd" ];
-        typescript =  [ "prettierd" ];
-        javascriptreact =  [ "prettierd" ];
-        typescriptreact =  [ "prettierd" ];
-        svelte =  [ "prettierd" ];
-        css =  [ "prettierd" ];
-        html =  [ "prettierd" ];
-        yaml =  [ "prettierd" ];
-        json =  [ "prettierd" ];
-        jsonc =  [ "prettierd" ];
-        markdown =  [ "prettierd" ];
-        graphql =  [ "prettierd" ];
-        lua =  [ "stylua" ];
-        python =  [ "ruff_format" ];
-        htmldjango =  [ "djlint" ];
-	nix = [ "nixfmt" ];
+        bash = ["shellcheck"];
+        sh = [ "shellharden" ];
+        javascript = [ "prettierd" ];
+        typescript = [ "prettierd" ];
+        javascriptreact = [ "prettierd" ];
+        typescriptreact = [ "prettierd" ];
+        svelte = [ "prettierd" ];
+        css = [ "prettierd" ];
+        html = [ "prettierd" ];
+        yaml = [ "prettierd" ];
+        json = [ "prettierd" ];
+        jsonc = [ "prettierd" ];
+        markdown = [ "prettierd" ];
+        graphql = [ "prettierd" ];
+        lua = [ "stylua" ];
+        python = [ "ruff_format" ];
+        nix = [ "nixfmt" ];
       };
 
       formatters = {
-	nixfmt = {
-	  command = lib.getExe pkgs.nixfmt-rfc-style;
-	};
+        shellcheck.command = lib.getExe pkgs.shellcheck;
+        shellharden.command = lib.getExe pkgs.shellharden;
+	stylua.command = lib.getExe pkgs.stylua;
+	ruff_format.command = lib.getExe pkgs.ruff_format;
+	prettierd.command = lib.getExe pkgs.prettierd;
+        nixfmt.command = lib.getExe pkgs.nixfmt-rfc-style;
       };
-      
+
       format_on_save = {
         lsp_fallback = true;
         async = false;
