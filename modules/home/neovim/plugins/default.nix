dc8e16538fb6baa5d610ba686e75a8e4044c89d2
danielscf <fdanielsantacruz@gmail.com>
2025-03-03 16:53:34 -0500

    Reorganizing imports

--- modules/home/neovim/plugins/default.nix
+++ modules/home/neovim/plugins/default.nix
@@ -1,37 +1,38 @@
 {
   imports = [
+    ./auto-session.nix
     ./better-escape.nix
     ./colorizer.nix
-    ./oil.nix
-    ./which-key.nix
-    ./auto-session.nix
-    ./tmux-navigator.nix
-    ./neoscroll.nix
-    ./harpoon.nix
-    ./luasnip.nix
-    ./lualine.nix
     ./dressing.nix
-    ./marks.nix
-    ./markdown-preview.nix
-    ./mini.nix
-    ./notify.nix
-    ./noice.nix
-    ./nvim-autopairs.nix
-    ./leap.nix
-    ./image.nix
-    ./obsidian.nix
-    ./trouble.nix
-    ./todo-comments.nix
-    ./vim-be-good.nix
     ./fugitive.nix
+    ./harpoon.nix
+    ./image.nix
+    ./leap.nix
+    ./lualine.nix
+    ./luasnip.nix
+    ./markdown-preview.nix
+    ./marks.nix
+    ./mini.nix
+    ./neoscroll.nix
+    ./noice.nix
+    ./notify.nix
+    ./nvim-autopairs.nix
+    ./obsidian.nix
+    ./oil.nix
     ./telescope.nix
+    ./tmux-navigator.nix
+    ./todo-comments.nix
+    ./trouble.nix
+    ./vim-be-good.nix
     ./web-devicons.nix
+    ./which-key.nix
 
-    ./lsp/lsp.nix
-    ./lsp/lint.nix
-    # ./lsp/java.nix
     ./lsp/blink-cmp.nix
     ./lsp/conform-nvim.nix
+    # ./lsp/dap.nix
+    # ./lsp/java.nix
+    ./lsp/lint.nix
+    ./lsp/lsp.nix
     ./lsp/treesitter.nix
     ./lsp/vim-dadbod.nix
   ];
