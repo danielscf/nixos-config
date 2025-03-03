fbb94a075aa3db741ae46e96fa79c914cc905a17
danielscf <fdanielsantacruz@gmail.com>
2025-03-03 16:42:19 -0500

    Nixvim lualine custom base16 theme

--- modules/home/neovim/plugins/lualine.nix
+++ modules/home/neovim/plugins/lualine.nix
@@ -2,6 +2,22 @@
   programs.nixvim.plugins.lualine = {
     enable = true;
 
+    luaConfig.post = ''
+      local base16_colors = require('base16-colorscheme').colors
+      local custom_base16 = require('lualine.themes.base16')
+
+      custom_base16.normal.a.bg = base16_colors.base00
+      custom_base16.normal.a.fg = base16_colors.base06
+
+      custom_base16.normal.b.bg = base16_colors.base00
+      custom_base16.normal.b.fg = base16_colors.base06
+
+      custom_base16.normal.c.bg = base16_colors.base00
+      custom_base16.normal.c.fg = base16_colors.base06
+
+      require('lualine').setup { options = { theme  = custom_base16 },}
+    '';
+
     settings = {
       options = {
         globalstatus = true;
