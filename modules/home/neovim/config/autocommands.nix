93ffade884cbc8f33eb9132fac681daaeb2503ff
danielscf <fdanielsantacruz@gmail.com>
2025-03-04 16:32:19 -0500

    Nixvim autocmd no line numbers on image buffers"

--- modules/home/neovim/config/autocommands.nix
+++ modules/home/neovim/config/autocommands.nix
@@ -1,24 +1,41 @@
 {
-  programs.nixvim.autoCmd = [
-    {
-      event = [ "BufEnter" ];
-      callback.__raw = ''
-        function ()
-          vim.opt.formatoptions:remove({ "c", "r", "o" })
-        end
-      '';
-      desc = "Disable new comment line";
-    }
-    {
-      event = [ "BufWinEnter" ];
-      callback.__raw = ''
-        function (event)
-          if vim.bo[event.buf].filetype == "help" then
-            vim.cmd.only()
+  programs.nixvim = {
+    autoGroups = {
+      ImageBuffer.clear = true;
+    };
+
+    autoCmd = [
+      {
+        event = [ "BufEnter" ];
+        callback.__raw = ''
+          function ()
+            vim.opt.formatoptions:remove({ "c", "r", "o" })
           end
-        end
-      '';
-      desc = "Open help page on a buffer";
-    }
-  ];
+        '';
+        desc = "Disable new comment line";
+      }
+      {
+        event = [ "BufWinEnter" ];
+        callback.__raw = ''
+          function (event)
+            if vim.bo[event.buf].filetype == "help" then
+              vim.cmd.only()
+            end
+          end
+        '';
+        desc = "Open help page on a buffer";
+      }
+      {
+        event = [ "BufReadPost" ];
+        group = "ImageBuffer";
+        pattern = [
+          "*.png"
+          ".jpeg"
+          "*.jpg"
+        ];
+        command = "setlocal nonumber norelativenumber";
+        desc = "Disable numberrs on image buffers";
+      }
+    ];
+  };
 }
