602653d246d9b06f3bcef892e1d5b3a211eec495
danielscf <fdanielsantacruz@gmail.com>
2025-03-03 23:03:33 -0500

    Nixvim temp disabled luasnip mappings

--- modules/home/neovim/plugins/luasnip.nix
+++ modules/home/neovim/plugins/luasnip.nix
@@ -3,57 +3,57 @@
     plugins.luasnip = {
       enable = true;
       filetypeExtend = {
-        typescript = ["tsdoc"];
-        javascript = ["jsdoc"];
-        lua = ["luadoc"];
-        python = ["pydoc"];
-        rust = ["rustdoc"];
-        java = ["javadoc"];
-        c = ["cdoc"];
-        cpp = ["cppdoc"];
-        php = ["phpdoc"];
-        kotlin = ["kdoc"];
-        ruby = ["rdoc"];
-        sh = ["shelldoc"];
+        typescript = [ "tsdoc" ];
+        javascript = [ "jsdoc" ];
+        lua = [ "luadoc" ];
+        python = [ "pydoc" ];
+        rust = [ "rustdoc" ];
+        java = [ "javadoc" ];
+        c = [ "cdoc" ];
+        cpp = [ "cppdoc" ];
+        php = [ "phpdoc" ];
+        kotlin = [ "kdoc" ];
+        ruby = [ "rdoc" ];
+        sh = [ "shelldoc" ];
       };
 
       settings = {
       };
     };
 
-    keymaps = [
-      {
-        key = "<Tab>";
-        action = ''
-          require("luasnip").expand_or_jump()
-        '';
-        mode = ["i"];
-        options.silent = true;
-      }
-      {
-        key = "<Tab>";
-        action = ''
-          require("luasnip").jump(1)
-        '';
-        mode = ["i" "s"];
-        options.silent = true;
-      }
-      {
-        key = "<S-Tab>";
-        action = ''
-          require("luasnip").jump(-1)
-        '';
-        mode = ["i" "s"];
-        options.silent = true;
-      }
-      {
-        key = "<C-E>";
-        action = ''
-          require("luasnip").change_choice(1)
-        '';
-        mode = ["i" "s"];
-        options.silent = true;
-      }
-    ];
+    # keymaps = [
+    #   {
+    #     key = "<Tab>";
+    #     action = ''
+    #       require("luasnip").expand_or_jump()
+    #     '';
+    #     mode = ["i"];
+    #     options.silent = true;
+    #   }
+    #   {
+    #     key = "<Tab>";
+    #     action = ''
+    #       require("luasnip").jump(1)
+    #     '';
+    #     mode = ["i" "s"];
+    #     options.silent = true;
+    #   }
+    #   {
+    #     key = "<S-Tab>";
+    #     action = ''
+    #       require("luasnip").jump(-1)
+    #     '';
+    #     mode = ["i" "s"];
+    #     options.silent = true;
+    #   }
+    #   {
+    #     key = "<C-E>";
+    #     action = ''
+    #       require("luasnip").change_choice(1)
+    #     '';
+    #     mode = ["i" "s"];
+    #     options.silent = true;
+    #   }
+    # ];
   };
 }
