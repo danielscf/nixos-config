af28e10c73d8b68eefe0393ed5ccb40b18384882
danielscf <fdanielsantacruz@gmail.com>
2025-03-05 18:16:29 -0500

    Grub quiet boot

--- modules/core/grub.nix
+++ modules/core/grub.nix
@@ -1,13 +1,17 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
   # TODO: Ask if efi/bios
   # TODO: Ask if encryption
   # TODO: Ask if OSProber
+  # TODO: Ask if quiet boot
   boot = {
+    consoleLogLevel = 0;
+    initrd.verbose = false;
     kernelParams = [
       "quiet"
-      "loglevel=3"
+      "udev.log_level=3"
     ];
+
     loader = {
       grub = {
         enable = true;
@@ -15,7 +19,9 @@
         useOSProber = true;
         efiSupport = true;
         enableCryptodisk = false;
+        theme = "${pkgs.catppuccin-grub}";
       };
+
       efi = {
         canTouchEfiVariables = true;
         efiSysMountPoint = "/boot";
