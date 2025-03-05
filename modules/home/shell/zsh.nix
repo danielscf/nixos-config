d6d1759a3524e4c9cbef9d1d9157203e0721a896
danielscf <fdanielsantacruz@gmail.com>
2025-03-05 12:29:55 -0500

    Zsh configuration

--- modules/home/shell/zsh.nix
+++ modules/home/shell/zsh.nix
@@ -1,5 +1,30 @@
 {
   programs.zsh = {
     enable = true;
+
+    defaultKeymap = "viins";
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    sessionVariables = {
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+      SUDO_EDITOR = "nvim";
+    };
+
+    shellAliases = {
+      "ls" = "eza -1";
+      "ll" = "eza -alh";
+      "tree" = "eza --tree";
+
+      "cat" = "bat";
+      "man" = "batman";
+
+      "trash" = "gio trash";
+
+      # TODO: Change for the current profile
+      "re" = "sudo nixos-rebuild switch --flake ~/nixos-config#work";
+      "ruh" = "sudo nixos-rebuild switch --upgrade-all --flake ~/nixos-config#work";
+    };
   };
 }
