658a4589291815aea3262bc8eaac88863152ec72
danielscf <fdanielsantacruz@gmail.com>
2025-03-04 20:07:11 -0500

    Nixvim which-key groups

--- modules/home/neovim/plugins/which-key.nix
+++ modules/home/neovim/plugins/which-key.nix
@@ -5,6 +5,41 @@
     settings = {
       delay = 500;
       preset = "modern";
+      icons.mappings = false;
+      spec = [
+        {
+          __unkeyed-1 = "<leader>f";
+          group = "File";
+        }
+        {
+          __unkeyed-1 = "<leader>b";
+          group = "Buffer";
+        }
+        {
+          __unkeyed-1 = "<leader>e";
+          group = "Editor";
+        }
+        {
+          __unkeyed-1 = "<leader>g";
+          group = "Git";
+        }
+        {
+          __unkeyed-1 = "<leader>l";
+          group = "Language";
+        }
+        {
+          __unkeyed-1 = "<leader>n";
+          group = "Notifications";
+        }
+        {
+          __unkeyed-1 = "<leader>s";
+          group = "Search";
+        }
+        {
+          __unkeyed-1 = "<leader>t";
+          group = "Trouble";
+        }
+      ];
     };
   };
 }
