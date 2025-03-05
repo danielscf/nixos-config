7da28657b7c7bfd3a77f8b3dcbf522a0c19cf756
danielscf <fdanielsantacruz@gmail.com>
2025-03-04 20:07:23 -0500

    Nixvim todo-comments telescope keymap

--- modules/home/neovim/plugins/todo-comments.nix
+++ modules/home/neovim/plugins/todo-comments.nix
@@ -11,6 +11,12 @@
         mode = [ "n" ];
         options.desc = "Todo-comments: Open todos using trouble";
       }
+      {
+        key = "<leader>tO";
+        action = "<cmd>TodoTelescope<cr>";
+        mode = [ "n" ];
+        options.desc = "Todo-comments: Open todos using telescope";
+      }
     ];
   };
 }
