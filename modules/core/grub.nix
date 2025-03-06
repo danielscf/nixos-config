64065489b891839290b3bf01b98ee87788b686d6
danielscf <fdanielsantacruz@gmail.com>
2025-03-06 00:11:55 -0500

    Grub plymouth

--- modules/core/grub.nix
+++ modules/core/grub.nix
@@ -7,24 +7,39 @@
   boot = {
     consoleLogLevel = 0;
     initrd.verbose = false;
+    plymouth.enable = true;
+
     kernelParams = [
       "quiet"
+      "splash"
+      "loglevel=3"
+      "i915.fastboot=1"
+      "plymouth.use-simpledrm"
+      "boot.shell_on_fail"
       "udev.log_level=3"
+      "udev.log_priority=3"
+      "systemd.show_status=false"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
     ];
 
     loader = {
-      grub = {
-        enable = true;
-        device = "nodev";
-        useOSProber = true;
-        efiSupport = true;
-        enableCryptodisk = false;
-        theme = "${pkgs.catppuccin-grub}";
-      };
+      timeout = null;
 
       efi = {
         canTouchEfiVariables = true;
         efiSysMountPoint = "/boot";
+      };
+
+      grub = {
+        enable = true;
+        device = "nodev";
+        efiSupport = true;
+        useOSProber = true;
+        enableCryptodisk = false;
+
+        splashImage = null;
+        theme = "${pkgs.catppuccin-grub}";
       };
     };
   };
