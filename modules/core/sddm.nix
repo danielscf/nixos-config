a7792eac9d429fd9e865a6788b07b6039955933a
danielscf <fdanielsantacruz@gmail.com>
2025-03-06 14:25:36 -0500

    Custom derivation for sddm theme

--- modules/core/sddm.nix
+++ modules/core/sddm.nix
@@ -1,9 +1,21 @@
-{ config, pkgs, ... }:
-
+{ pkgs, ... }:
 {
+  environment.systemPackages = [
+    (pkgs.where-is-my-sddm-theme.override {
+      themeConfig.General.passwordCursorColor = "#FFFFFF";
+    })
+    (pkgs.pkgs.callPackage ./custom_derivations/sddm-astronaut-theme.nix {
+      theme = "black_hole";
+    })
+  ];
+
   services.displayManager.sddm = {
     enable = true;
-    # TODO: Ask if wayland should be enable
     wayland.enable = true;
+
+    package = pkgs.kdePackages.sddm;
+
+    # theme = "where_is_my_sddm_theme";
+    theme = "sddm-astronaut-theme";
   };
 }
