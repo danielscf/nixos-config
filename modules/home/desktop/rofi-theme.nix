28d4ffe9a84906a1abc663eede853d4b3cd51f7c
danielscf <fdanielsantacruz@gmail.com>
2025-03-07 17:06:52 -0500

    Rofi theme

--- modules/home/desktop/rofi-theme.nix
+++ modules/home/desktop/rofi-theme.nix
@@ -215,5 +215,99 @@
         background-color = mkLiteral "var(selected-normal-background)";
         text-color = mkLiteral "var(selected-normal-foreground)";
       };
+      "element selected.urgent" = {
+        background-color = mkLiteral "var(selected-urgent-background)";
+        text-color = mkLiteral "var(selected-urgent-foreground)";
+      };
+      "element selected.active" = {
+        background-color = mkLiteral "var(selected-active-background)";
+        text-color = mkLiteral "var(selected-active-foreground)";
+      };
+      "element alternate.normal" = {
+        background-color = mkLiteral "var(alternate-normal-background)";
+        text-color = mkLiteral "var(alternate-normal-foreground)";
+      };
+      "element alternate.urgent" = {
+        background-color = mkLiteral "var(alternate-urgent-background)";
+        text-color = mkLiteral "var(alternate-urgent-foreground)";
+      };
+      "element alternate.active" = {
+        background-color = mkLiteral "var(alternate-active-background)";
+        text-color = mkLiteral "var(alternate-active-foreground)";
+      };
+      "element-icon" = {
+        background-color = mkLiteral "transparent";
+        text-color = mkLiteral "inherit";
+        size = mkLiteral "24px";
+        cursor = mkLiteral "inherit";
+      };
+      "element-text" = {
+        background-color = mkLiteral "transparent";
+        text-color = mkLiteral "inherit";
+        highlight = mkLiteral "inherit";
+        cursor = mkLiteral "inherit";
+        vertical-align = mkLiteral "0.5";
+        horizontal-align = mkLiteral "0.0";
+      };
+
+      # ****----- Mode Switcher -----****
+      "mode-switcher" = {
+        enabled = true;
+        spacing = mkLiteral "10px";
+        margin = mkLiteral "0px";
+        padding = mkLiteral "0px";
+        border = mkLiteral "0px solid";
+        border-radius = mkLiteral "0px";
+        border-color = mkLiteral "@border-colour";
+        background-color = mkLiteral "transparent";
+        text-color = mkLiteral "@foreground-colour";
+      };
+      "button" = {
+        padding = mkLiteral "5px 10px";
+        border = mkLiteral "0px solid";
+        border-radius = mkLiteral "10px";
+        border-color = mkLiteral "@border-colour";
+        background-color = mkLiteral "@alternate-background";
+        text-color = mkLiteral "inherit";
+        cursor = mkLiteral "pointer";
+      };
+      "button selected" = {
+        background-color = mkLiteral "var(selected-normal-background)";
+        text-color = mkLiteral "var(selected-normal-foreground)";
+      };
+
+      # ****----- Message -----****
+      "message" = {
+        enabled = true;
+        margin = mkLiteral "0px";
+        padding = mkLiteral "0px";
+        border = mkLiteral "0px solid";
+        border-radius = mkLiteral "0px 0px 0px 0px";
+        border-color = mkLiteral "@border-colour";
+        background-color = mkLiteral "transparent";
+        text-color = mkLiteral "@foreground-colour";
+      };
+      "textbox" = {
+        padding = mkLiteral "8px 10px";
+        border = mkLiteral "0px solid";
+        border-radius = mkLiteral "10px";
+        border-color = mkLiteral "@border-colour";
+        background-color = mkLiteral "@alternate-background";
+        text-color = mkLiteral "@foreground-colour";
+        vertical-align = mkLiteral "0.5";
+        horizontal-align = mkLiteral "0.0";
+        highlight = mkLiteral "none";
+        placeholder-color = mkLiteral "@foreground-colour";
+        blink = true;
+        markup = true;
+      };
+      "error-message" = {
+        padding = mkLiteral "10px";
+        border = mkLiteral "2px solid";
+        border-radius = mkLiteral "10px";
+        border-color = mkLiteral "@border-colour";
+        background-color = mkLiteral "@background-colour";
+        text-color = mkLiteral "@foreground-colour";
+      };
     };
 }
--- modules/home/desktop/rofi.nix
+++ modules/home/desktop/rofi.nix
@@ -4,6 +4,10 @@
   ...
 }:
 {
+  imports = [
+    ./rofi-theme.nix
+  ];
+
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
@@ -25,107 +29,6 @@
       rofi-emoji-wayland
     ];
 
-    theme = import ./rofi-theme.nix;
-
-    # theme =
-    #   let
-    #     inherit (config.lib.formats.rasi) mkLiteral;
-    #   in
-    #   {
-    #     "*" = {
-    #       # font = mkLiteral "Roboto 12";
-    #       background-color = mkLiteral "transparent";
-    #       text-color = mkLiteral "#E6E6E6";
-    #       margin = mkLiteral "0px";
-    #       padding = mkLiteral "0px";
-    #       spacing = mkLiteral "0px";
-    #     };
-    #
-    #     "window" = {
-    #       location = mkLiteral "north";
-    #       y-offset = mkLiteral "calc(50% - 176px)";
-    #       width = 480;
-    #       border-radius = mkLiteral "24px";
-    #       background-color = mkLiteral "#212121F2";
-    #     };
-    #
-    #     "mainbox" = {
-    #       padding = mkLiteral "12px";
-    #     };
-    #
-    #     "inputbar" = {
-    #       background-color = mkLiteral "#2A2A2A";
-    #       border-color = mkLiteral "#1A73E8F2";
-    #
-    #       border = mkLiteral "2px";
-    #       border-radius = mkLiteral "16px";
-    #       padding = mkLiteral "8px 16px";
-    #       spacing = mkLiteral "8px";
-    #       children = map mkLiteral [
-    #         "prompt"
-    #         "entry"
-    #       ];
-    #     };
-    #
-    #     "prompt" = {
-    #       text-color = mkLiteral "#969696";
-    #     };
-    #
-    #     "entry" = {
-    #       placeholder = "Search";
-    #       placeholder-color = mkLiteral "#3D3D3D";
-    #     };
-    #
-    #     "message" = {
-    #       margin = mkLiteral "12px 0 0";
-    #       border-radius = mkLiteral "16px";
-    #       border-color = mkLiteral "#3D3D3D80";
-    #       background-color = mkLiteral "#3D3D3D80";
-    #     };
-    #
-    #     "textbox" = {
-    #       padding = mkLiteral "8px 24px";
-    #     };
-    #
-    #     "listview" = {
-    #       background-color = mkLiteral "transparent";
-    #       margin = mkLiteral "12px 0 0";
-    #       lines = 8;
-    #       columns = 1;
-    #       fixed-height = false;
-    #     };
-    #
-    #     "element" = {
-    #       padding = mkLiteral "8px 16px";
-    #       spacing = mkLiteral "8px";
-    #       border-radius = mkLiteral "16px";
-    #     };
-    #
-    #     "element normal active" = {
-    #       text-color = mkLiteral "#1A73E8F2";
-    #     };
-    #
-    #     "element alternate active" = {
-    #       text-color = mkLiteral "#1A73E8F2";
-    #     };
-    #
-    #     "element selected normal" = {
-    #       background-color = mkLiteral "#1A73E8F2";
-    #     };
-    #
-    #     "element selected active" = {
-    #       background-color = mkLiteral "#1A73E8F2";
-    #     };
-    #
-    #     "element-icon" = {
-    #       size = mkLiteral "1em";
-    #       vertical-align = mkLiteral "0.5";
-    #     };
-    #
-    #     "element-text" = {
-    #       text-color = mkLiteral "inherit";
-    #     };
-    #   };
     # extraConfig = {
     # };
   };
